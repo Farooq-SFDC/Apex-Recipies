2dc44297d24d334de6e49211b22ce9efc9d14da8
Farooq-SCDC <farooq.knr2@gmail.com>
2023-09-28 15:41:14 +0530

    3:41 commit

--- force-app/main/default/lwc/chatBar/chatBar.js
+++ force-app/main/default/lwc/chatBar/chatBar.js
@@ -1,5 +1,12 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, api } from 'lwc';
 
 export default class ChatBar extends LightningElement {
-    
+    @api percentage;
+    handleChange(event) {
+        this.percentage = event.detail.value;
+    }
+
+    get style(){
+        return `width: ${this.percentage}%`
+    }
 }
